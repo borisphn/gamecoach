b0cd90f9041761fa79900a4fa89bab1a64510907
smoopa <thegreatrishikumar@gmail.com>
2018-01-20 18:31:26 -0500

    monkeys

---
+++ Assets/iwillmove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iwillmove : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime*200);
+            transform.Rotate(Vector3.right * Time.deltaTime*200);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-1*Vector3.forward * Time.deltaTime*200);
+            transform.Rotate(-1*Vector3.right * Time.deltaTime*200);
+        }
+    }
+}
